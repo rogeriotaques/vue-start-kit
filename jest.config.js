2c25ca2bf1246b30fde5b9aec8f2100c9fa5a8a9
Rogerio Taques <rogerio.taques@gmail.com>
2020-12-03 02:26:00 +0900

    chore(house keeping): settings

--- jest.config.js
+++ jest.config.js
@@ -1,20 +1,15 @@
 module.exports = {
   verbose: true,
-  moduleFileExtensions: ["ts", "js", "json", "vue"],
+  moduleFileExtensions: ['ts', 'js', 'json', 'vue'],
   transform: {
-    ".*\\.(ts|js)$": "babel-jest",
-    ".*\\.(ts)$": "ts-jest",
-    ".*\\.(vue)$": "vue-jest"
+    '.*\\.(ts|js)$': 'babel-jest',
+    '.*\\.(ts)$': 'ts-jest',
+    '.*\\.(vue)$': 'vue-jest'
   },
   moduleNameMapper: {
-    "^~/(.*)$": "<rootDir>/src/$1",
-    "^vue$": "vue/dist/vue.esm.js"
+    '^~/(.*)$': '<rootDir>/src/$1',
+    '^vue$': 'vue/dist/vue.esm.js'
   },
   collectCoverage: false,
-  collectCoverageFrom: [
-    "src/**/*.{ts,js,vue}",
-    "!**/node_modules/**",
-    "!**/coverage/**",
-    "!**/dist/**"
-  ]
+  collectCoverageFrom: ['src/**/*.{ts,js,vue}', '!**/node_modules/**', '!**/coverage/**', '!**/dist/**']
 };
