3368f531bb73f9b8e6489da388f6a9f395a9aa8c
Rogério Taques <rogerio.taques@gmail.com>
2021-10-11 11:22:43 +0900

    chore: replacing parcel with webpack (#23)

--- src/domain/router.ts
+++ src/domain/router.ts
@@ -1,28 +1,15 @@
-/**
- * Vue Start Kit
- *
- * Where all the routes are defined.
- *
- * @author Rogerio Taques
- * @license MIT
- */
-
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 
-import TaskList from '~/components/tasks/task-list.vue';
+import TaskList from '~/components/TaskList.vue';
 
 Vue.use(VueRouter);
 
 const routes = [
-  { path: '/', component: TaskList }
-
-  // Additional paths are coming here ...
-  // { path: '/about', component: SampleAbout },
-  // ...
-
-  // Also possible to create a catch-all page (error page?)
-  // { path: '/*', component: SampleError404 }
+  {
+    path: '/',
+    component: TaskList
+  }
 ];
 
 export const router = new VueRouter({
